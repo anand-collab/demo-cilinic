0a5b56d046bef11103127b49aa574f6da0db632c
anand-collab <95568946+anand-collab@users.noreply.github.com>
2026-05-22 14:36:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -272,7 +272,7 @@ export default function App() {
       </section>
 
       {/* --- Appointment & Map --- */}
-      <section id="appointment" className="py-24 bg-white relative">
+     <section id="appointment" className="py-24 bg-transparent relative">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid lg:grid-cols-2 gap-16 bg-white rounded-3xl overflow-hidden shadow-2xl border border-gray-100">
             
